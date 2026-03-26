fc3d952a9a4d73dfd4981886caa5edda853575fb
JacobH-droid <39959@ocvts.org>
2026-03-26 14:20:52 -0400

    Create Weapons.py

---
+++ Weapons.py
@@ -0,0 +1,21 @@
+# weapons.py
+
+weapons = {
+    "Iron Sword": {
+        "attack": 5,
+        "speed": 2,
+        "type": "melee"
+    },
+
+    "Battle Axe": {
+        "attack": 8,
+        "speed": 1,
+        "type": "melee"
+    },
+
+    "Elven Bow": {
+        "attack": 6,
+        "speed": 3,
+        "type": "ranged"
+    }
+}

04049321b946f833f7cd237f33ed845368ed6a48
JacobH-droid <39959@ocvts.org>
2026-03-26 14:21:19 -0400

    Update quests.py

--- quests.py
+++ quests.py
@@ -1,1 +1,16 @@
+# quests.py
 
+quests = {
+    "Darkwood Forest": {
+        "description": "Clear the forest of goblins.",
+        "stages": [
+            {"enemy": "Goblin", "count": 3},
+            {"enemy": "Orc Warrior", "count": 1}
+        ],
+        "rewards": {
+            "XP": 50,
+            "Gold": 30,
+            "Items": ["Health Potion"]
+        }
+    }
+}

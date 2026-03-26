eb9d0e954a0f8a25726f36b5d65371eb138da7a7
JacobH-droid <39959@ocvts.org>
2026-03-26 14:19:26 -0400

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -1,1 +1,33 @@
+# enemies.py
 
+enemies = {
+    "Goblin": {
+        "HP": 20,
+        "Attack": 5,
+        "Defense": 2,
+        "Speed": 5,
+        "XP": 10,
+        "Gold": 5,
+        "loot_table": ["Health Potion"]
+    },
+
+    "Orc Warrior": {
+        "HP": 35,
+        "Attack": 8,
+        "Defense": 4,
+        "Speed": 3,
+        "XP": 20,
+        "Gold": 15,
+        "loot_table": ["Iron Sword", "Steel Shield"]
+    },
+
+    "Dark Mage": {
+        "HP": 25,
+        "Attack": 10,
+        "Defense": 3,
+        "Speed": 4,
+        "XP": 30,
+        "Gold": 20,
+        "loot_table": ["Mana Potion"]
+    }
+}

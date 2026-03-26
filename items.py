a4a7ed5c19e0d2389280617928054659db8fcc76
JacobH-droid <39959@ocvts.org>
2026-03-26 14:20:31 -0400

    Update items.py

--- items.py
+++ items.py
@@ -2,25 +2,6 @@
 
 items = {
     # ---------------------------
-    # WEAPONS
-    # ---------------------------
-    "Iron Sword": {
-        "type": "weapon",
-        "attack": 5,
-        "range": "melee",
-        "rarity": "common",
-        "value": 10
-    },
-
-    "Longbow": {
-        "type": "weapon",
-        "attack": 4,
-        "range": "ranged",
-        "rarity": "common",
-        "value": 12
-    },
-
-    # ---------------------------
     # ARMOR
     # ---------------------------
     "Steel Shield": {
